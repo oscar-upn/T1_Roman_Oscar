811698be691caa69af0201aecb323ada4f9f1a03
n00350433 <n00350433@upn.pe>
2025-06-21 16:54:19 -0500

    Creacion Libro.cs

---
+++ T1_Roman_Oscar/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace T1_Roman_Oscar.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ T1_Roman_Oscar/Models/Libro.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace T1_Roman_Oscar.Models
+{
+    public class Libro
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Titulo { get; set; }
+        public string Autor {  get; set; }
+        public string Tema { get; set; }
+        public string Editorial { get; set; }
+        public int AnioPublicacion { get; set; }
+        public int Paginas {  get; set; }
+        public string Categoria { get; set; }
+        public string Material {  get; set; }
+        public int Copias { get; set; }
+
+        public string Descripcion { get; set; }
+    }
+}
